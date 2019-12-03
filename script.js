3536b8155436b1d6658b2ef0ecc58afe057585d2
jeffreytram <jeffrey_tram@yahoo.com>
2019-12-03 00:13:55 -0500

    Added animation gallery

--- script.js
+++
@@ -1,8 +0,0 @@
-(function (l) { 
-    var i, s = { 
-        touchend: function () { 
-
-        } }; 
-        for (i in s) 
-            l.addEventListener(i, s) 
-    })(document); // sticky hover fix in iOS
